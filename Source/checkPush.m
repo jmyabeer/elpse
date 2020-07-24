150bed64da43c561b6e3eeff634ea5cd83a881b0
Jason Myatt <myatt@ualberta.ca>
2020-07-24 13:48:54 -0600

    Added documentation and pointed out a logic error

--- Source/checkPush.m
+++ Source/checkPush.m
@@ -1,14 +1,27 @@
 function haltFlags = checkPush(rayBundle,refracAllow)
-
-% checkPush determines whether each ray in the given ray bundle has been 
+% function haltFlags = checkPush(rayBundle,refractAllow)
+%
+%      rayBundle - a ray bundle (no assumption on type)
+%   refractAllow - the amount in degrees in change between
+%                  wavevectors for successive trajectory entries that triggers 
+%                  the halt flag to be set (i.e. halt if the change
+%                  is less than this.    
+%
+% Steven: checkPush determines whether each ray in the given ray bundle has been 
 % pushed far enough so that the change in direction is within a given angle
 % of allowance. If the ray's change in direction is within the allowance,
 % a 1 will be put in the position in haltFlags corresponding to that ray.
 % If the ray's change in direction is not within the allowance, a 0 will be
 % put in the position in haltFlags corresponding to that ray.
-
 % Currently will set halt flag if one of the changes in direction is more
 % than the allowed change
+%
+%   JFM comment (24/July/2020): we should change this a bit. It
+%   should reflect the amount of change per unit time (since the
+%   successive trajectory entries could be very close together in
+%   time).
+%   Also - it should return a logical vector.
+    
 
     % default angle of allowance
     if ~exist('refracAllow','var')
@@ -23,9 +36,12 @@ function haltFlags = checkPush(rayBundle,refracAllow)
     
     % Calculates the change in angle between a position and the position
     % after
+    %
     diff = angle(2:end,:) - angle(1:end-1,:);
     under = diff < refracAllow;
+    
     % Sets halt flag if any change in direction is less than the allowed
     % change in direction
+    %
     haltFlags = under(1,:)+under(2,:)+under(3,:)+under(4,:);
 end
